b1b176f33d0aec47d2eda3f8082a3d7e8c893f6d
henzel lopez <elgatolocopro98@gmail.com>
2023-09-19 07:53:58 -0600

    agregando ejercicio 2

---
+++ ejercicios/ejercicio-2/mensajes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{ 
+    string nombre; 
+
+    cout <<"ingrese su nombre :";
+    cin >>nombre;
+
+cout << endl;
+    cout <<"hola mi nombre es"<< nombre;
+cout << endl;
+    return 0;
+}
